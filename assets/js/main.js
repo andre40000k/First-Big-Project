1d75f3e89351816b32b0b49a636220c54d79c060
Andrii <andre40000k@gmail.com>
2024-10-16 15:24:13 +0300

    finished this project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
-console.log('Hello world');
+$(document).ready(function () {
+  $('.header-navbar__btn').on('click', function () {
+    $(this).toggleClass('active')
+    $('.header-navbar__list').stop(true, true).slideToggle(500)
+  })
+})
